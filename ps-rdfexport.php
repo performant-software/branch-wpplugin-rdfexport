45792f98e314505fee16c2ea223bdf9ef8db55c7
Dave Goldstein <dave@performantsoftware.com>
2012-11-08 11:26:15 -0500

    Refactored XML generation and moving on to the actual content

--- ps-rdfexport.php
+++ ps-rdfexport.php
@@ -4,6 +4,8 @@
  *
  */
 
+require_once( ABSPATH . "wp-includes/pluggable.php" );
+
 if( isset( $_GET['rdfdownload'] ) ) {
    rdf_export( );
    exit;
@@ -19,30 +21,21 @@ function rdf_export( ) {
    header( 'Content-Disposition: attachment; filename=' . $filename );
    header( 'Content-Type: text/xml; charset=' . get_option( 'blog_charset' ), true );
 
-   rdf_header( );
+   rdf_header_contents( );
 
-   global $post;
-   $args = array( 'numberposts' => 10 );
-   $posts = get_posts( $args );
-   foreach( $posts as $post ) {
-      /* setup_postdata( $post ); */
-      process_post( $post );
+   /* global $post; */
+   $qargs = array( 'post_type' => 'ps_articles',
+                   'posts_per_page' => -1 );
+   $query= new WP_Query( $qargs );
+   while ( $query->have_posts( ) ) {
+      $query->the_post( );
+      $post = $query->post;
+      /* $query->next_post( ); */
+      rdf_object_contents( $post );
    }
+
    wp_reset_postdata( );
-   rdf_footer( );
-}
-
-function rdf_header( ) {
-?>
-<!-- Set up namespaces: it isn't necessary to reference an actual XSD schema to validate the RDF-use the "xmlns" value only to establish a unique namespace-->
-<rdf:RDF>
-<?php
-}
-
-function rdf_footer( ) {
-?>
-</rdf:RDF>
-<?php
+   rdf_footer_contents( );
 }
 
 function process_post( $post ) {
@@ -91,4 +84,183 @@ function process_post( $post ) {
    </branch:object>
 <?php
 }
+
+function rdf_object_contents( $post ) {
+   $tag = "branch:object";
+   $indent = 1;
+   rdf_open_tag( $tag, $indent );
+   rdf_newline( );
+   $indent = 2;
+   rdf_archive_contents( $post, $indent );
+   rdf_federation_contents( $post, $indent );
+   rdf_see_also_contents( $post, $indent );
+   rdf_title_contents( $post, $indent );
+   rdf_author_contents( $post, $indent );
+   rdf_editor_contents( $post, $indent );
+   rdf_publisher_contents( $post, $indent );
+   rdf_translator_contents( $post, $indent );
+   rdf_dc_date_contents( $post, $indent );
+   rdf_genre_contents( $post, $indent );
+   rdf_discipline_contents( $post, $indent );
+   rdf_type_contents( $post, $indent );
+   rdf_text_contents( $post, $indent );
+   rdf_thumbnail_contents( $post, $indent );
+   $indent = 1;
+   rdf_close_tag( $tag, $indent );
+}
+
+function rdf_archive_contents( $post, $indent ) {
+   $tag = "collex:archive";
+   rdf_open_tag( $tag, $indent );
+   echo "branch";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_federation_contents( $post, $indent ) {
+   $tag = "collex:federation";
+   rdf_open_tag( $tag, $indent );
+   echo "NINES";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_see_also_contents( $post, $indent ) {
+   $tag = "rdfs:seeAlso";
+   rdf_open_tag( $tag, $indent );
+   echo "XXX SEE ALSO XXX";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_title_contents( $post, $indent ) {
+   $tag = "dc:title";
+   rdf_open_tag( $tag, $indent );
+   echo $post["post_title"];
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_author_contents( $post, $indent ) {
+   $tag = "role:AUT";
+   rdf_open_tag( $tag, $indent );
+   echo "XXX ROLE AUT XXX";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_editor_contents( $post, $indent ) {
+   $tag = "role:EDT";
+   rdf_open_tag( $tag, $indent );
+   echo "XXX ROLE EDT XXX";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_publisher_contents( $post, $indent ) {
+   $tag = "role:PBL";
+   rdf_open_tag( $tag, $indent );
+   echo "RaVoN";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_translator_contents( $post, $indent ) {
+   $tag = "role:TRL";
+   rdf_open_tag( $tag, $indent );
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_dc_date_contents( $post, $indent ) {
+   $tag = "dc:date";
+   rdf_open_tag( $tag, $indent );
+   rdf_newline( );
+   rdf_colex_date_contents( $post, $indent + 1 );
+   rdf_close_tag( $tag, $indent );
+}
+
+function rdf_colex_date_contents( $post, $indent ) {
+   $tag = "colex:date";
+   rdf_open_tag( $tag, $indent );
+   rdf_newline( );
+   rdf_date_label_contents( $post, $indent + 1 );
+   rdf_date_value_contents( $post, $indent + 1 );
+   rdf_close_tag( $tag, $indent );
+}
+
+function rdf_date_label_contents( $post, $indent ) {
+   $tag = "rdfs:label";
+   rdf_open_tag( $tag, $indent );
+   echo "XXX DATE LABEL XXX";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_date_value_contents( $post, $indent ) {
+   $tag = "rdf:value";
+   rdf_open_tag( $tag, $indent );
+   echo "XXX DATE VALUE XXX";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_genre_contents( $post, $indent ) {
+   $tag = "collex:genre";
+   rdf_open_tag( $tag, $indent );
+   echo "Criticism";
+   rdf_close_tag( $tag, 0 );
+   rdf_open_tag( $tag, $indent );
+   echo "Nonfiction";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_discipline_contents( $post, $indent ) {
+   $tag = "colex:discipline";
+   rdf_open_tag( $tag, $indent );
+   echo "History";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_type_contents( $post, $indent ) {
+   $tag = "dc:type";
+   rdf_open_tag( $tag, $indent );
+   echo "InteractiveResource";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_text_contents( $post, $indent ) {
+   $tag = "collex:text";
+   rdf_open_tag( $tag, $indent );
+   echo "XXX TEXT XXX";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_thumbnail_contents( $post, $indent ) {
+   $tag = "collex:thumbnail";
+   rdf_open_tag( $tag, $indent );
+   echo "XXX THUMBNAIL XXX";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_header_contents( ) {
+   $tag = "rdf:RDF";
+   rdf_open_tag( $tag, 0 );
+   rdf_newline( );
+}
+
+function rdf_footer_contents( ) {
+   $tag = "rdf:RDF";
+   rdf_close_tag( $tag, 0 );
+}
+
+function rdf_open_tag( $name, $indent ) {
+  $indent_str = str_repeat( "   ", $indent );
+  echo "$indent_str<$name>";
+}
+
+function rdf_close_tag( $name, $indent ) {
+  $indent_str = str_repeat( "   ", $indent );
+  echo "$indent_str</$name>";
+  rdf_newline( );
+}
+
+function rdf_newline( ) {
+   echo "\n";
+}
+
+function basic_log( $msg ) {
+   file_put_contents( "/tmp/debug.log", "$msg\n" );
+}
+
 ?>
